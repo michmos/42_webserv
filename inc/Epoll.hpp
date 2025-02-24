8fa5c44db45aec510b2888c5bf3c1087cf52b677
Michael Moser <Michael.Moser11@web.de>
2025-02-24 18:13:11 +0100

    Fix epoll class

--- inc/Epoll.hpp
+++ inc/Epoll.hpp
@@ -11,9 +11,12 @@
 #define	TIMEOUT_DFLT 3
 #define	MAX_EVENTS 5
 
+// since Linux 2.6.8 ignored anyways
+#define	EPOLL_START_SIZE 5
+
 class Epoll {
 public:
-	Epoll(int size);
+	Epoll();
 	Epoll(const Epoll &toCopy) = delete;
 	Epoll& operator=(const Epoll& toAssign) = delete;
 	~Epoll() noexcept;
@@ -21,8 +24,11 @@ public:
 	void	add(int fd, u_int32_t events) const;
 	void	mod(int fd, u_int32_t events) const;
 	void	del(int fd) const;
-	void	wait();
+	int		wait();
 	void	setTimeout(int timeout) noexcept;
+
+	int								getEpFd() const;
+	const struct epoll_event* const	getEvents() const;
 	// TODO: maybe add pwait() from epoll_pwait()
 
 private:
