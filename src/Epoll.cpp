6a8f2fc788deec88ef767cc9fa53b58660fd274b
Michael Moser <Michael.Moser11@web.de>
2025-02-24 12:14:29 +0100

    Update err message

--- src/Epoll.cpp
+++ src/Epoll.cpp
@@ -6,7 +6,7 @@ Epoll::Epoll(int size) :
 	_timeout(TIMEOUT_DFLT)
 {
 	if (_epFd == -1) {
-		throw std::runtime_error(strerror(errno));
+		throw std::runtime_error(std::string("epoll_create()") + strerror(errno));
 	}
 }
 
@@ -16,7 +16,7 @@ void	Epoll::add(int fd, u_int32_t events) const {
 	ev.events = events;
 	ev.data.fd = fd;
 	if (epoll_ctl(_epFd, EPOLL_CTL_ADD, fd, &ev) == -1) {
-		throw std::runtime_error(strerror(errno));
+		throw std::runtime_error(std::string("epoll_ctl(add)") + strerror(errno));
 	}
 }
 
@@ -26,19 +26,19 @@ void	Epoll::mod(int fd, u_int32_t events) const {
 	ev.events = events;
 	ev.data.fd = fd;
 	if (epoll_ctl(_epFd, EPOLL_CTL_MOD, fd, &ev) == -1) {
-		throw std::runtime_error(strerror(errno));
+		throw std::runtime_error(std::string("epoll_ctl(mod)") + strerror(errno));
 	}
 }
 
 void	Epoll::del(int fd) const {
 	if (epoll_ctl(_epFd, EPOLL_CTL_DEL, fd, nullptr) == -1) {
-		throw std::runtime_error(strerror(errno));
+		throw std::runtime_error(std::string("epoll_ctl(del)") + strerror(errno));
 	}
 }
 
 void	Epoll::wait() {
 	if (epoll_wait(_epFd, _events, MAX_EVENTS, _timeout) == -1) {
-		throw std::runtime_error(strerror(errno));
+		throw std::runtime_error(std::string("epoll_wait()") + strerror(errno));
 	}
 }
 
