3e3929b40abf16e95ec74cc7a8f9dd0b6f9883f4
Michael Moser <Michael.Moser11@web.de>
2025-02-26 18:11:03 +0100

    Add client class

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,21 +1,47 @@
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <iostream>
-#include "../inc/Socket.hpp"
 
-int	main() {
-	try {
-		Socket	server;
+#include "../inc/Client.hpp"
+#include <stdexcept>
 
-		server.sConnect(inet_addr("127.0.0.1"), htons(8080));
-		const char *msg = "hello world";
-		write(server.getFd(), msg, strlen(msg));
-		std::cout << "message written" << std::endl;
-		close(server.getFd());
-	} catch (...) {
-		std::cerr << "Error occurred" << std::endl;
+
+Client::Client(int fd) : _fd(fd), _status(DFLT) {
+}
+
+tStatus Client::getStatus() const {
+	return (_status);
+}
+
+void Client::setStatus(tStatus newStatus) {
+	_status = newStatus;
+}
+
+int Client::getFd() const {
+	return (_fd);
+}
+
+void	Client::readFrom() {
+	this->setStatus(READING);
+
+	char buff[READSIZE + 1] = { '\0'};
+	int bRead = read(_fd, buff, READSIZE);
+	if (bRead == -1) {
+		throw std::runtime_error(std::string("read(): ") + strerror(errno));
 	}
-	return (0);
+	_buff.append(buff);
+
+	if (bRead < READSIZE) {
+		this->setStatus(FINISHED_READING);
+		#ifdef DEBUG
+		std::cout << "Full string: " << _buff << std::endl;
+		#endif
+	}
+	#ifdef DEBUG
+	std::cout << "Received: " << buff << std::endl;
+	#endif
+}
+
+void	Client::writeTo() {
+}
+
+
+Client::~Client() {
 }
