d7405be98dc9c4937b94d6ca98652050c236d9f3
Michael Moser <Michael.Moser11@web.de>
2025-02-26 13:52:49 +0100

    Add test main

--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,68 @@
 #include <iostream>
+#include <stdexcept>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/epoll.h>
 #include <fcntl.h>
 #include <sys/types.h>
+#include <string.h>
 
-// size of event buffer for epoll_wait
-#define MAX_EVENTS 10
-
-#define EP_WAIT_TIMEOUT 5
-
-
-// maybe throw exception instead
-int	setFdFlag(int fd, int flag) {
+void	setFdFlag(int fd, int flag) {
 	int flags = fcntl(fd, F_GETFL);
 	if (fcntl(fd, F_SETFL, flags | flag) == -1)
-		return (-1);
-	return (0);
+		throw std::runtime_error(std::string("fcntl()") + strerror(errno));
 }
 
-// throw exceptions
-int	createListenSock(void) {
-	int	listen_sock;
-
-	if((listen_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-		perror("socket()");
-		return (EXIT_FAILURE);
-	}
-	if (setFdFlag(listen_sock, O_NONBLOCK) == -1) {
-		return (EXIT_FAILURE);
-	}
-
-	struct sockaddr_in	srvrAddr;
-	srvrAddr.sin_family = AF_INET;
-	srvrAddr.sin_addr.s_addr = INADDR_ANY;
-	srvrAddr.sin_port = htons(8080);
-	if (bind(listen_sock, (const struct sockaddr*)&srvrAddr, sizeof(srvrAddr)) == -1) {
-		perror("socket()");
-		return (EXIT_FAILURE);
-	}
-
-	// TODO: double check backlog size
-	if(listen(listen_sock, 5) == -1) {
-		perror("listen()");
-		return (EXIT_FAILURE);
-	}
-	return (listen_sock);
-}
-
-// throw exceptions
-int	epoll_add(int epFd, int fd, u_int32_t events) {
-	epoll_event	ev;
-
-	ev.events = events;
-	ev.data.fd = fd;
-	if (epoll_ctl(epFd, EPOLL_CTL_ADD, fd, &ev) == -1) {
-		perror("epoll_clt(EPOLL_CTL_ADD)");
-		return (EXIT_FAILURE);
-	}
-	return (0);
-}
+#include "../inc/Socket.hpp"
+#include "../inc/Epoll.hpp"
 
 int	main() {
-	int listen_sock = createListenSock();
-	// TODO: double check epoll indication size
-	int epFd = epoll_create(5);
-	epoll_add(epFd, listen_sock, EPOLLIN);
+	try {
+		Socket	listen;
+		Epoll	ep;
 
-	while (true) {
-		epoll_event	events[MAX_EVENTS];
-		int ready = epoll_wait(epFd, events, MAX_EVENTS, EP_WAIT_TIMEOUT);
-		if (ready == -1) {
-			perror("epoll_wait()");
-			return (EXIT_FAILURE);
-		}
-		for (int i = 0; i < ready; ++i) {
-			if (events[i].data.fd == listen_sock) {
-				int newClient = accept(listen_sock, nullptr, nullptr);
-				if (newClient == -1) {
-					perror("accept()");
-					return (EXIT_FAILURE);
-				}
-				epoll_add(epFd, newClient, EPOLLIN | EPOLLOUT);
-			} else {
-				// do something with existent client
+		listen.sBind(INADDR_ANY, htons(8080));
+		listen.sListen(5);
+		setFdFlag(listen.getFd(), O_NONBLOCK);
+		ep.add(listen.getFd(), EPOLLIN);
+
+		while (true) {
+			auto events = ep.wait();
+			for (auto ev : events) {
+				std::cout << "client: " << ev.data.fd << " "
+					<< ((ev.events & EPOLLIN) ? "EPOLLIN " : " ")
+					<< ((ev.events & EPOLLOUT) ? "EPOLLOUT " : " ")
+					<< ((ev.events & (EPOLLHUP | EPOLLERR)) ? "EPOLLHUP | EPOLLERR" : "") << std::endl;
+
+				if (ev.data.fd == listen.getFd()) {
+					int newClient = listen.sAccept();
+					ep.add(newClient, EPOLLIN | EPOLLOUT);
+					std::cout << "New client registered: " << newClient << std::endl;
+				} else {
+					// do someting wit existing client
+					if (ev.events & EPOLLIN) {
+						char buff[100] = { '\0' };
+						int bRead = read(ev.data.fd, buff, 100);
+						std::cout << "Received following message: " << buff << std::endl;
+						// TODO: check read -1
+						if (bRead < 100) {
+							ep.del(ev.data.fd);
+							close(ev.data.fd);
+							std::cout << "Client connection closed: " << ev.data.fd << std::endl;
+						}
+					} else if (ev.events & (EPOLLHUP | EPOLLERR)) {
+						ep.del(ev.data.fd);
+						close(ev.data.fd);
+						std::cout << "Client connection closed: " << ev.data.fd << std::endl;
+					} 				}
 			}
 		}
+	} catch (std::runtime_error &e) {
+		std::cerr << "Runtime error occurred: " << e.what() << std::endl;
+	} catch (std::invalid_argument &e) {
+		std::cerr << "Invalid argument error occurred: " << e.what() << std::endl;
+	} catch (...) {
+		std::cerr << "Other error occurred" << std::endl;
 	}
 
 	return (0);
