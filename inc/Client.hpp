aac24915579ca241eb686bb9e9531f378ca068d6
Michael Moser <Michael.Moser11@web.de>
2025-02-26 18:11:03 +0100

    Add client class

---
+++ inc/Client.hpp
@@ -0,0 +1,37 @@
+#ifndef CLIENT_HPP
+# define CLIENT_HPP
+
+#include <iostream>
+#include <unistd.h>
+#include <string.h>
+#define READSIZE	100
+
+typedef enum eStatus {
+	DFLT,
+	READING,
+	FINISHED_READING,
+	WRITING,
+	FINISHED_WRITING,
+	FINISHED
+} tStatus;
+
+class Client {
+public:
+	Client(int fd);
+	~Client();
+
+	void	readFrom();
+	void	writeTo();
+
+	tStatus	getStatus() const;
+	void	setStatus(tStatus newStatus);
+	int		getFd() const;
+
+private:
+	std::string	_buff;
+	tStatus		_status;
+	int			_fd;
+
+};
+
+#endif
