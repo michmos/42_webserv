01461b8ce92a5509d996d80eb50ddad114c617f3
Michael Moser <Michael.Moser11@web.de>
2025-03-06 15:42:40 +0100

    Rename to ptr

--- src/SharedFd.cpp
+++ src/SharedFd.cpp
@@ -20,13 +20,13 @@ int	UniqueFd::get() const {
 	return (_fd);
 }
 
-SharedFd::SharedFd() : _fd(std::make_shared<UniqueFd>(-1)) {
+SharedFd::SharedFd() : _fdPtr(std::make_shared<UniqueFd>(-1)) {
 }
 
 SharedFd::SharedFd(int fd) {
 	if (fd < 0)
 		throw std::invalid_argument("SharedFd(int): fd < 0");
-	_fd = std::make_shared<UniqueFd>(fd);
+	_fdPtr = std::make_shared<UniqueFd>(fd);
 }
 
 SharedFd::SharedFd(const SharedFd& other) {
@@ -36,18 +36,17 @@ SharedFd::SharedFd(const SharedFd& other) {
 }
 
 SharedFd& SharedFd::operator=(const SharedFd& other) {
-	this->_fd = other._fd;
+	this->_fdPtr = other._fdPtr;
 	return(*this);
 }
 
-// TODO: maybe delete this overload - see whether actually makes sense
 SharedFd& SharedFd::operator=(int fd) {
-	this->_fd = std::make_shared<UniqueFd>(fd);
+	this->_fdPtr = std::make_shared<UniqueFd>(fd);
 	return(*this);
 }
 
 bool	SharedFd::operator==(const SharedFd& other) {
-	return (this->_fd == other._fd);
+	return (this->_fdPtr == other._fdPtr);
 }
 
 SharedFd::~SharedFd() {
@@ -63,12 +62,11 @@ void	SharedFd::setNonBlock() const {
 }
 
 bool	SharedFd::isValid() const {
-	return (_fd->get() >= 0);
+	return (_fdPtr->get() >= 0);
 }
 
 int	SharedFd::get() const {
 	if (!this->isValid())
 		throw std::runtime_error("SharedFd.get(): _fd not set");
-	return(_fd->get());
-
+	return(_fdPtr->get());
 }
