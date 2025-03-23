05070cad3bb592bec85b03128f404f56faf52858
Michael Moser <Michael.Moser11@web.de>
2025-03-23 13:44:55 +0100

    implement SharedFd in Socket

--- src/Socket.cpp
+++ src/Socket.cpp
@@ -4,64 +4,54 @@
 #include <string>
 
 Socket::Socket() : 
-	_fd(socket(AF_INET, SOCK_STREAM, 0))
+	_fd(::socket(AF_INET, SOCK_STREAM, 0))
 {
 	if (_fd == -1) {
 		throw std::runtime_error(std::string("socket(): ") + strerror(errno));
 	}
 }
 
-Socket::Socket(int sockFd) : 
-	_fd(sockFd)
-{
-	if (_fd < 0) {
-		throw std::invalid_argument(std::string("Socket(int) constructor called with ") + std::to_string(sockFd));
-	}
-}
-
 Socket::~Socket() {
-	close(_fd);
 }
 
-void	Socket::sBind(in_addr_t ipv4Addr, in_port_t port) const {
+void	Socket::bind(in_addr_t ipv4Addr, in_port_t port) {
 	struct sockaddr_in addr;
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = ipv4Addr;
 	addr.sin_port = port;
 
-	if (bind(_fd, (const struct sockaddr*)&addr, sizeof(addr)) == -1) {
+	if (::bind(_fd.get(), (const struct sockaddr*)&addr, sizeof(addr)) == -1) {
 		throw std::runtime_error(std::string("bind(): ") + strerror(errno));
 	}
 }
 
-void	Socket::sListen(int backlog) const {
-	if (listen(_fd, backlog) == -1) {
+void	Socket::listen(int backlog) {
+	if (::listen(_fd.get(), backlog) == -1) {
 		throw std::runtime_error(std::string("listen(): ") + strerror(errno));
 	}
 }
 
-int	Socket::sAccept() const {
+int	Socket::accept() {
 	int	newClient;
 
-	if ((newClient = accept(_fd, nullptr, nullptr)) == -1) {
+	if ((newClient = ::accept(_fd.get(), nullptr, nullptr)) == -1) {
 		throw std::runtime_error(std::string("accept(): ") + strerror(errno));
 	}
 	return (newClient);
 }
 
-void	Socket::sConnect(in_addr_t ipv4Addr, in_port_t port) const {
+void	Socket::connect(in_addr_t ipv4Addr, in_port_t port) {
 	struct sockaddr_in addr;
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = ipv4Addr;
 	addr.sin_port = port;
 
-	if (connect(_fd, (const struct sockaddr*)&addr, sizeof(addr)) == -1) {
+	if (::connect(_fd.get(), (const struct sockaddr*)&addr, sizeof(addr)) == -1) {
 		throw std::runtime_error(std::string("connect(): ") + strerror(errno));
 	}
 }
 
-int	Socket::getFd() const {
+const SharedFd&	Socket::getFd() const {
 	return (_fd);
 }
-
 
