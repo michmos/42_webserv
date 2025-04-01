e032f84e014e131d221c732ecfee18a9834c3075
Merel <mvan-ess@codam.student.nl>
2025-04-01 09:46:55 +0200

    save main

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,37 @@
 #include "../inc/Webserv/Webserv.hpp"
 
-// int	main() {
-// 	try {
-// 		// Server	server(INADDR_ANY, htons(8080));
-		
-// 		// server.runServer();
+int	save_main(int argc, char **argv) {
+	std::string path = "configs/";
 
-// 	} catch (std::runtime_error &e) {
-// 		std::cerr << "Runtime error occurred: " << e.what() << std::endl;
-// 	} catch (std::invalid_argument &e) {
-// 		std::cerr << "Invalid argument error occurred: " << e.what() << std::endl;
-// 	} catch (...) {
-// 		std::cerr << "Other error occurred" << std::endl;
-// 	}
-
-// 	return (0);
-// }
+	if (argc == 1)
+		path += "default.conf";
+	else if (argc == 2)
+		path += argv[1];
+	else
+	{
+		std::cerr << "Wrong amount of config files given" << std::endl;
+		return (1);
+	}
+	Webserv	server(path);
+	return (0);
+}
 
 int main (int argc, char **argv) {
-	std::string path;
-
-	try
-	{
-		std::cerr << argc << std::endl;
-		if (argc == 1)
-			path = "configs/default.conf";
-		else if (argc == 2)
-			path = argv[1];
-		else
-		{
-			std::cerr << "Wrong amount of config files given" << std::endl;
-			return (1);
-		}
-		Webserv	server(path);
+	try {
+		return (save_main(argc, argv));
 	}
-	catch (std::runtime_error &e)
-	{
+	catch (std::runtime_error &e) {
 		std::cerr << "Runtime error occurred: " << e.what() << std::endl;
 	}
 	catch (std::invalid_argument &e) {
 		std::cerr << "Invalid argument error occurred: " << e.what() << std::endl;
 	}
-	catch (std::exception &e)
-	{
+	catch (std::exception &e) {
 		std::cerr << "Error occurred: " << e.what() << std::endl;
 	}
 	catch (...)
 	{
 		std::cout << "something went wrong" << std::endl;
-		return (1);
 	}
-	return (0);
+	return (01);
 }
