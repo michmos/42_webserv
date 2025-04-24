308043eaae81939ae89a375d453c2a1f139519e8
Michael Moser <Michael.Moser11@web.de>
2025-04-24 18:14:02 +0200

    Fix print in test_siege

--- test_siege.sh
+++ test_siege.sh
@@ -1,3 +1,4 @@
 echo "" > test_results.txt
-echo -e "TEST: ${YEL} Stresstest with siege -r 10000 -c 42 http://localhost:8080/index.html: ${RES}"
+echo -e "TEST: ${YEL} Stresstest with siege -r 7000 -c 22 http://localhost:8080/index.html: ${RES}"
 siege -r 7000 -c 22 http://localhost:8080/http/empty.html
+echo ""
